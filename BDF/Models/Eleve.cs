c0d7a8cbbe1ec492a973ff5ae9c85b9a9d78f4e8
{agathelepri} <{alepri@ensc.fr}>
2025-01-21 14:41:04 +0100

    API

---
+++ BDF/Models/Eleve.cs
@@ -0,0 +1,14 @@
+public class Eleve
+{
+    public int Id {get;set;}
+    public string Nom {get;set;}
+    public string Prenom {get;set;}
+    public string Login {get;set;}
+    public string MDP {get;set;}
+    public Promotion IdPromotion {get;set;}
+    public Famille IdFamille {get;set;} = null!;
+    public byte[] Photo { get; set; }
+    public Eleve IdEleveParrain {get;set;} = null!;
+
+    public Eleve(){}
+}
