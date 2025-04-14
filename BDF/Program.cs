902c81108e714d66a47af63d5ddaed9eaa336f82
{agathelepri} <{alepri@ensc.fr}>
2025-04-14 10:01:14 +0200

    fin

--- BDF/Program.cs
+++ BDF/Program.cs
@@ -1,83 +1,33 @@
-/* var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
-
-app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
- */
-using BDF.Data; // Accès à SeedData
+using BDF.Data; 
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Initialiser les données après la configuration de la base
 builder.Services.AddDbContext<DataContext>();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// Configuration CORS pour autoriser le frontend React et Swagger
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAll",
-        policy => policy.WithOrigins("http://localhost:3000") // Autorise React
-                        .AllowAnyMethod()  // GET, POST, PUT, DELETE...
-                        .AllowAnyHeader()); // Autorise tous les headers
+        policy => policy.WithOrigins("http://localhost:3000") 
+                        .AllowAnyMethod()  
+                        .AllowAnyHeader()); 
 });
 
 var app = builder.Build();
 
-// Assurer l'initialisation des données APRÈS la configuration des services
 //SeedData.Init();
 
-// Activer Swagger uniquement en mode développement
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-// Configuration du pipeline des middlewares
 app.UseHttpsRedirection();
-app.UseCors("AllowAll"); // Appliquer la politique CORS
+app.UseCors("AllowAll");
 app.UseAuthorization();
 app.MapControllers();
 app.Run();
